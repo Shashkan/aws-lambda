b73bcaf8e0a48776e4db411c0068d412b7ca8dbd
Shashkan <136038009+Shashkan@users.noreply.github.com>
2023-07-25 15:07:15 -0400

    Trasnfer files

--- bin/lamda-code.ts
+++ bin/lamda-code.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { LamdaCodeStack } from '../lib/lamda-code-stack';
+import * as cdk from '@aws-cdk/core';
+import { CdkWorkshopStack } from '../lib/lamda-code-stack';
 
 const app = new cdk.App();
-new LamdaCodeStack(app, 'LamdaCodeStack', {
+new CdkWorkshopStack(app, 'LamdaCodeCFN');
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +18,3 @@ new LamdaCodeStack(app, 'LamdaCodeStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
--- lib/lamda-code-stack.ts
+++ lib/lamda-code-stack.ts
@@ -1,16 +1,21 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
-export class LamdaCodeStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkWorkshopStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+ // defines an AWS Lambda resource
+ const hello = new lambda.Function(this, 'HelloHandler', {
+  runtime: lambda.Runtime.NODEJS_16_X,    // execution environment
+  code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+  handler: 'hello.handler'                // file is "hello", function is "handler"
+});
 
-    // The code that defines your stack goes here
+// defines an API Gateway REST API resource backed by our "hello" function.
+new apigw.LambdaRestApi(this, 'Endpoint', {
+  handler: hello
+});
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LamdaCodeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+}
 }
